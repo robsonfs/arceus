aa0cfd3ec66520648c8ece97c2129126f261a1e7
Robson Santos <robfds@gmail.com>
2023-02-23 22:50:29 -0300

    A tiny and naive implementation of the client side!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Result, Context, Ok};
+use hyper::{Body, Client};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let https = hyper_rustls::HttpsConnectorBuilder::new()
+        .with_native_roots()
+        .https_only()
+        .enable_http1()
+        .build();
+
+    let client: Client<_, Body> = Client::builder().build(https);
+    let url = "https://pokeapi.co/api/v2/pokemon/arceus"
+        .parse()
+        .context("Parsing URL")?;
+    let resp = client.get(url)
+        .await
+        .context("Performing an HTTP request")?;
+    println!("{:#?}", resp);
+
+    Ok(())
 }
